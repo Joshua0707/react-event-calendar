5f6be0519df5a198f74d0a087a68968c2f277797
Joshua0707 <oladapoodelola@gmail.com>
2020-09-19 17:04:20 +0100

    Updates

---
+++ precache-manifest.547a738da07d6f4170d8bccb9cf34ec5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e222c955f1e5bc85679d43b8d65ef6",
+    "url": "/react-event-calendar/index.html"
+  },
+  {
+    "revision": "0a3b8c6daa77dc66cd55",
+    "url": "/react-event-calendar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ea0b6960a03b9f4bf65",
+    "url": "/react-event-calendar/static/js/2.71b521e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-event-calendar/static/js/2.71b521e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3b8c6daa77dc66cd55",
+    "url": "/react-event-calendar/static/js/main.84a0373f.chunk.js"
+  },
+  {
+    "revision": "634b3b459c105ef98696",
+    "url": "/react-event-calendar/static/js/runtime-main.4fcc98f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-event-calendar/static/media/logo.5d5d9eef.svg"
+  }
+]);
